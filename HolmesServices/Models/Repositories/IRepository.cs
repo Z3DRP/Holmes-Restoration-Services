a4f8a9033f9ca42b5db427f89c8ff0b9b24fc986
Zach Palmer <apex1421@outlook.com>
2022-06-13 20:21:41 -0500

    Irepo sett up

---
+++ HolmesServices/Models/Repositories/IRepository.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+
+namespace HolmesServices.Models.Repositories
+{
+    public interface IRepository<T> where T : class
+    {
+        IEnumerable<T> List(QueryOptions<T> options);
+
+        T Get(QueryOptions<T> options);
+        T Get(int id);
+        T Get(string id);
+        // pass in a objcect
+        T Get(T obj);
+
+        void Insert(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+
+        void Save();
+    }
+}
---
+++ HolmesServices/Models/Repositories/Repository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using HolmesServices.Models.Repositories;
+
+namespace HolmesServices.Models.Repositories
+{
+    public class Repository<T> 
+    {
+
+
+    }
+}
