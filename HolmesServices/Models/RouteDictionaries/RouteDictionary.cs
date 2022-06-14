6013c8d8579c294f283ca0a8449fc9b565e78a11
Zach Palmer <apex1421@outlook.com>
2022-06-13 20:01:25 -0500

    list view models done

---
+++ HolmesServices/Models/RouteDictionaries/RouteDictionary.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using HolmesServices.Models.DTOs;
+using HolmesServices.Models.ExtensionMethods;
+
+namespace HolmesServices.Models.RouteDictionaries
+{
+    public static class FilterPrefix
+    {
+        public const string Type = "type-";
+        public const string Price = "price-";
+    }
+    public class RouteDictionary : Dictionary<string, string>
+    {
+        public int PageNumber
+        {
+            get => Get(nameof(GridDTO.PageNumber)).ToInt();
+            set => this[nameof(GridDTO.PageNumber)] = value.ToString();
+        }
+        public int PageSize
+        {
+            get => Get(nameof(GridDTO.PageSize)).ToInt();
+            set => this[nameof(GridDTO.PageSize)] = value.ToString();
+        }
+        public string SortField
+        {
+            get => Get(nameof(GridDTO.SortField));
+            set => this[nameof(GridDTO.SortField)] = value;
+        }
+        public string SortDirection
+        {
+            get => Get(nameof(GridDTO.SortDirection));
+            set => this[nameof(GridDTO.SortDirection)] = value;
+        }
+        public void SetSortAndDirection(string fieldName, RouteDictionary current)
+        {
+            if (current.SortField.EqualsNoCase(fieldName) && current.SortDirection == "asc")
+                this[nameof(GridDTO.SortDirection)] = "desc";
+            else
+                this[nameof(GridDTO.SortDirection)] = "asc";
+        }
+        public string DeckTypeFilter
+        {
+            get => Get(nameof(DeckingGridDTO.Type))?.Replace(FilterPrefix.Type, "");
+            set => this[nameof(DeckingGridDTO.Type)] = value;
+        }
+        public string RailTypeFilter
+        {
+            get => Get(nameof(RailingGridDTO.Type))?.Replace(FilterPrefix.Type, "");
+            set => this[nameof(RailingGridDTO.Type)] = value;
+        }
+        public string DeckPriceFilter
+        {
+            get => Get(nameof(DeckingGridDTO.Price))?.Replace(FilterPrefix.Price, "");
+            set => this[nameof(DeckingGridDTO.Price)] = value;
+        }
+        public string RailPriceFilter
+        {
+            get => Get(nameof(RailingGridDTO.Price))?.Replace(FilterPrefix.Price, "");
+            set => this[nameof(RailingGridDTO.Price)] = value;
+        }
+        public void ClearFilters() =>
+            DeckTypeFilter = RailTypeFilter = DeckPriceFilter = RailPriceFilter = RailingGridDTO.DefaultFilter;
+
+        private string Get(string key) => Keys.Contains(key) ? this[key] : null;
+
+        // return a new dictionary that contains the same values as this dictionary.
+        // needed so that pages can change the route values when calculating paging, sorting,
+        // and filtering links, without changing the values of the current route
+        public RouteDictionary Clone()
+        {
+            var clone = new RouteDictionary();
+            foreach (var key in Keys)
+            {
+                clone.Add(key, this[key]);
+            }
+            return clone;
+        }
+    }
+}
--- HolmesServices/ViewModels/DesignListViewModel.cs
+++ HolmesServices/ViewModels/DesignListViewModel.cs
@@ -2,10 +2,18 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using HolmesServices.Models;
+using HolmesServices.Models.RouteDictionaries;
 
 namespace HolmesServices.ViewModels
 {
     public class DesignListViewModel
     {
+        public IEnumerable<Design> Designs { get; set; }
+        public RouteDictionary CurrentRoute { get; set; }
+        public int TotalPages { get; set; }
+        // data for filter drop-downs 
+        public Customer Customer { get; set; }
+
     }
 }
